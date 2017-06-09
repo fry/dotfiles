d11d0017e84175e591e143642e40221a759aaac2
Eike Siewertsen <eike@technocreatives.com>
2017-06-09 09:37:57 +0200

    add vscode

--- mb.sh
+++ mb.sh
@@ -16,3 +16,7 @@ $LN $PWD/src/.tmux.conf ~/.tmux.conf
 # zsh
 $LN $PWD/src/zsh/.inputrc ~/.inputrc
 
+# vscode
+$LN $PWD/src/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
+$LN $PWD/src/vscode/snippets/ ~/Library/Application\ Support/Code/User/snippets
+
