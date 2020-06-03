b2a13e8d9953a87d5588adb50db5cfb828ba5a96
Eike Siewertsen <eike@technocreatives.com>
2020-06-03 19:23:15 +0200

    windows

--- mb.sh
+++ mb.sh
@@ -10,13 +10,15 @@ $LN $PWD/src/emacs/.emacs.d ~/.emacs.d
 $LN $PWD/src/vim/.vimrc ~/.vimrc
 $LN $PWD/src/vim/.vim ~/.vim
 
-# tmu
+# tmux
 $LN $PWD/src/tmux/tmux.conf ~/.tmux.conf
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm ; true
 
 # zsh
 $LN $PWD/src/zsh/zshrc ~/.zshrc
 $LN $PWD/src/zsh/inputrc ~/.inputrc
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
 
 # vscode
 $LN $PWD/src/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
@@ -25,6 +27,8 @@ $LN $PWD/src/vscode/snippets/ ~/Library/Application\ Support/Code/User/snippets
 # kitty
 mkdir -p ~/.config/kitty
 $LN $PWD/src/kitty/kitty.conf ~/.config/kitty/kitty.conf
+
+fi
 
 # git
 $LN $PWD/src/git/.gitconfig ~/.gitconfig
